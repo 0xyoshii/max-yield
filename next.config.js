ad4acbd0a96553ac157065f1796d728009984b05
Yoshi <teomendix@gmail.com>
2025-02-20 22:55:43 -0500

    PLESAE WORK

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    ppr: true,
+    serverComponentsExternalPackages: ["twitter-api-v2"]
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'avatar.vercel.sh',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig 
